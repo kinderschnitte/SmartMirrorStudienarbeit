fa26ca81bcfbdb612a7d1c720bdf710a740e6016
Tim Retzlaff <retzlaff-tim@gmx.de>
2018-09-23 20:04:53 +0200

    Log hinzugefügt.

---
+++ SmartMirror/Log/Log.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using Windows.Storage;
+
+namespace Log
+{
+    public static class Log
+    {
+        static Log()
+        {
+            CreateLogFile();
+        }
+
+        private static async void CreateLogFile()
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+
+            await storageFolder.CreateFileAsync("log.txt", CreationCollisionOption.OpenIfExists);
+        }
+
+        public static async void WriteLog(Exception exception)
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile logFile = await storageFolder.GetFileAsync("log.txt");
+
+            StringBuilder message = new StringBuilder();
+
+            message.AppendLine("<---------------" + DateTime.Now.ToString("G") + "--------------->");
+            message.AppendLine("Message: " + exception.Message);
+            message.AppendLine("Stacktrace: " + exception.StackTrace);
+            message.AppendLine("");
+
+            await FileIO.WriteTextAsync(logFile, message.ToString());
+        }
+    }
+}
